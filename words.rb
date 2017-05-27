bc7387b8e2fdddb08225083f2bbcbac2543764d0
PauladO <pauladenouden@gmail.com>
2017-05-27 12:18:41 +0200

    Add anagram logic, refactor

--- words.rb
+++ words.rb
@@ -3,14 +3,29 @@ words = File.read('wordlist.txt')
 words = words.downcase
 words = words.split("\n")
 
+starting_time = Time.now
+sorted_words = Hash.new
 
-
-
-
-def is_anagram?(a, b)
-  return true
+words.each do |word|
+  sorted_words[word.chars.sort.join] ||= []
+  sorted_words[word.chars.sort.join].push(word)
 end
 
-puts is_anagram?('test', 'stet')
+anagrams = sorted_words.select { |key, value|   value.length > 1 }
 
-puts is_anagram?('test', 'mest')
+##################################################################
+
+puts "It took #{(Time.now - starting_time).round(2)} seconds to compile a list of #{anagrams.count} sets of anagrams."
+
+continue = "y"
+
+while continue == "y" || continue == "yes" do
+  print "which word are you looking for? "
+  anagram_word = gets.chomp
+
+  answer = anagrams["#{anagram_word.chars.sort.join}"].select {|word| word != anagram_word}
+  answer ||= []
+  puts answer.length == 0 ? "No anagrams found" : answer.join(", ")
+  puts "Need more anagrams? y/n"
+  continue = gets.chomp.downcase
+end
